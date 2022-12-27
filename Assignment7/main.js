67fbdb99542621dc4ff3499b37ad26a57146593d
Vartikasaxena99 <vartika.saxena_cs19@gla.ac.in>
2022-12-27 15:44:46 +0530

    commit

---
+++ Assignment7/main.js
@@ -0,0 +1,16 @@
+const black=document.getElementById("h1");
+function changeColor(color) {
+        document.body.style.background = color;
+}
+function d1()
+{
+    changeColor('black');
+}
+function d2()
+{
+    changeColor('yellow');
+}
+function d3()
+{
+    changeColor('green');
+}
